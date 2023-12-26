7bcd80b7ccc0809416914c6bd5ac98b81e03bea7
HFTlab <arbitragecryptobotnet@gmail.com>
2023-12-26 13:00:54 +0100

    fixed all_clients typemisses

--- core/all_clients.py
+++ core/all_clients.py
@@ -5,12 +5,11 @@ from clients.dydx import DydxClient
 from clients.kraken import KrakenClient
 from clients.okx import OkxClient
 from clients.hitbtc import HitbtcClient
-from clients.bibox import BiboxClient
 from clients.bitmake import BitmakeClient
 from clients.globederivative import GlobeClient
 from clients.bit import BitClient
 from clients.btse import BtseClient
-from clients.whitebit import WhiteBit
+from clients.whitebit import WhiteBitClient
 
 ALL_CLIENTS = {
     'BITMEX': BitmexClient,
@@ -20,10 +19,8 @@ ALL_CLIENTS = {
     'OKX': OkxClient,
     'KRAKEN': KrakenClient,
     'HITBTC': HitbtcClient,
-    'BIBOX': BiboxClient,
     'BITMAKE': BitmakeClient,
     'GLOBE': GlobeClient,
     'BIT': BitClient,
     'BTSE': BtseClient,
-    'WHITEBIT': WhiteBit,
-    'KRAKEN': KrakenClient}
+    'WHITEBIT': WhiteBitClient}
