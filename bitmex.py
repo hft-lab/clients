5c472b14c4225507818ed88fb13aaf773da4f9e8
HFTlab <arbitragecryptobotnet@gmail.com>
2023-12-27 16:14:51 +0100

    fixed get_order_by_id input

--- bitmex.py
+++ bitmex.py
@@ -227,7 +227,7 @@ class BitmexClient(BaseClient):
         return orders
 
     @try_exc_regular
-    def get_order_by_id(self, symbol, order_id, session):
+    def get_order_by_id(self, symbol, order_id):
         res = self.swagger_client.Order.Order_getOrders(filter=json.dumps({'orderID': order_id})).result()[0][0]
         contract_value = self.get_contract_value(symbol)
         real_size = res['cumQty'] / contract_value
