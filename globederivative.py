0405b2b278d88bad26c00f0b244dc26603faf352
Dima T <115705736+Zimrathon@users.noreply.github.com>
2024-01-16 21:58:23 +0700

    Доработки globe под парсер

--- globederivative.py
+++ globederivative.py
@@ -16,10 +16,11 @@ class GlobeClient:
     BASE_URL = 'https://globe.exchange'
     EXCHANGE_NAME = 'GLOBE'
 
-    def __init__(self, keys=None, leverage=None, state='Bot', markets_list=[], max_pos_part=20, finder=None, ob_len=4):
+    def __init__(self, multibot=None, keys=None, leverage=None, state='Bot', markets_list=[], max_pos_part=20, finder=None, ob_len=4):
+        self.multibot = multibot
+        self.state = state
         self.finder = finder
         self.ob_len = ob_len
-        self.state = state
         if self.state == 'Bot':
             self.api_key = keys['API_KEY']
             self.api_secret = keys['API_SECRET']
@@ -29,7 +30,7 @@ class GlobeClient:
         self._loop_public = asyncio.new_event_loop()
         self._connected = asyncio.Event()
         self.wst_public = threading.Thread(target=self._run_ws_forever, args=[self._loop_public])
-        self._wst_orderbooks = threading.Thread(target=self._process_ws_line)
+        # self._wst_orderbooks = threading.Thread(target=self._process_ws_line)
         self.message_queue = asyncio.Queue(loop=self._loop_public)
         self.orderbook = {}
         self.positions = {}
@@ -111,20 +112,37 @@ class GlobeClient:
 
     @try_exc_async
     async def _run_ws_loop(self):
+        i = 0
         async with aiohttp.ClientSession() as s:
             async with s.ws_connect(self.PUBLIC_WS_ENDPOINT) as ws:
                 self._connected.set()
                 self._ws_public = ws
                 for symbol in self.markets.values():
                     asyncio.run_coroutine_threadsafe(self.subscribe_orderbooks(symbol), self._loop_public)
+
                 async for msg in ws:
-                    await self.message_queue.put(msg)
+                    i += 1
+                    print(f'{i=}')
+                    # msg = await self.message_queue.get()
+                    data = json.loads(msg.data)
+                    # self.update_orderbook(json.loads(msg.data))
+                    ts_ob = data['data']['timestamp'] / 1000
+                    print(data)
+                    ts_ms = time.time()
+                    print(ts_ms - ts_ob)
+                    # await self.message_queue.put(msg)
 
     @try_exc_async
     async def process_messages(self):
         while True:
             msg = await self.message_queue.get()
-            self.update_orderbook(json.loads(msg.data))
+            data = json.loads(msg.data)
+            # self.update_orderbook(json.loads(msg.data))
+            ts_ob = data['data']['timestamp']/1000
+            # print(data)
+            ts_ms = time.time()
+            print(ts_ms-ts_ob)
+
 
     @try_exc_regular
     def get_orderbook(self, symbol):
@@ -145,24 +163,39 @@ class GlobeClient:
         await self._ws_public.send_json(method)
 
     @try_exc_regular
-    def update_orderbook(self, data):
+    async def update_orderbook(self, data):
+        flag = False
+
         if not data.get('data'):
             return
         market = data['subscription']['instrument']
         if not self.orderbook.get(market):
             self.orderbook.update({market: {'asks': [], 'bids': []}})
-        snap = self.orderbook[market].copy()
+        new_ob = self.orderbook[market].copy()
+        ts_ms = time.time()
+        new_ob['ts_ms'] = ts_ms
+        ts_ob = data['data']['timestamp']
+        if isinstance(ts_ob, int):
+            ts_ob = ts_ob / 1000
+        new_ob['timestamp'] = ts_ob
         self.orderbook[market].update({'asks': [[x['price'], x['volume']] for x in data['data']['asks']],
                                        'bids': [[x['price'], x['volume']] for x in data['data']['bids']],
                                        'timestamp': data['data']['timestamp'],
                                        'ts_ms': time.time()})
-        if self.finder and snap['asks']:
-            if_new_top_ask = snap['asks'][0][0] > self.orderbook[market]['asks'][0][0]
-            if_new_top_bid = snap['bids'][0][0] < self.orderbook[market]['bids'][0][0]
-            if if_new_top_ask or if_new_top_bid:
-                coin = market.split('USDT')[0]
-                self.finder.coins_to_check.add(coin)
-                self.finder.update = True
+        if self.finder and new_ob['asks']:
+            if_new_top_ask = new_ob['asks'][0][0] > self.orderbook[market]['asks'][0][0]
+            flag = True
+            side = 'buy'
+        if self.finder and new_ob['bids']:
+            if_new_top_bid = new_ob['bids'][0][0] < self.orderbook[market]['bids'][0][0]
+            flag = True
+            side = 'sell'
+        # if flag and ts_ms - ts_ob < 1:
+        #     coin = market.split('USDT')[0]
+        #     if self.state == 'Bot':
+        #         await self.finder.count_one_coin(coin, self.EXCHANGE_NAME, side, self.multibot.run_arbitrage)
+        #     else:
+        #         await self.finder.count_one_coin(coin, self.EXCHANGE_NAME, side)
 
     @try_exc_regular
     def __generate_signature(self, data):
@@ -172,15 +205,21 @@ class GlobeClient:
     def run_updater(self):
         self.wst_public.daemon = True
         self.wst_public.start()
-        self._wst_orderbooks.daemon = True
-        self._wst_orderbooks.start()
+        # self._wst_orderbooks.daemon = True
+        # self._wst_orderbooks.start()
 
 
 if __name__ == '__main__':
-    client = GlobeClient()
+    import configparser
+
+    config = configparser.ConfigParser()
+    config.read('config_parser.ini', "utf-8")
+
+    keys = config['GLOBE']
+    client = GlobeClient(keys=keys, state='Parser')
     # print(client.get_markets())
     client.run_updater()
-    while True:
-        time.sleep(5)
-        print(client.get_all_tops())
+    # while True:
+    #     time.sleep(1)
+    #     print(client.get_all_tops())
 
